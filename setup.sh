710e2cc4667d2c631386d353a38680138c21f042
p-chandra <33384192+p-chandra@users.noreply.github.com>
2020-09-10 22:57:29 -0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#install curl
+sudo apt install curl
+
 #install vpn
 sudo apt install network-manager-openconnect network-manager-openconnect-gnome
 sudo systemctl restart network-manager.service
@@ -9,15 +12,23 @@ sudo apt-get install gcc-multilib
 
 #install vim
 sudo apt install vim
+
 #install git
 sudo apt install git
+
 #install nmap
 sudo apt install nmap
 
+#install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+python3 -m pip install --upgrade pip
+
+#install pwntools
+pip3 install pwntools
 
 #install pwndbg
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
 ./setup.sh
-
 
