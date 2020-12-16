5331ca376787a4d4d004aabf6d3623d1e9606115
p-chandra <33384192+p-chandra@users.noreply.github.com>
2020-12-15 19:43:13 -0800

    Update setup.sh

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 #Set your clock time based on internet
-sudo apt-get install ntp
+sudo apt-get install ntp -y
 
 #update your distro
-sudo apt-get -u dist-upgrade
+sudo apt-get -u dist-upgrade -y
 
 #install curl
-sudo apt install curl
+sudo apt install curl -y
 
 #install vpn
-sudo apt install network-manager-openconnect network-manager-openconnect-gnome
-sudo systemctl restart network-manager.service
+sudo apt install network-manager-openconnect network-manager-openconnect-gnome -y
+sudo systemctl restart network-manager.service -y
 
 #install 32bit libraries
 sudo apt-get install gcc-multilib
 
 #install vim
-sudo apt install vim
+sudo apt install vim -y
 
 #install git
-sudo apt install git
+sudo apt install git -y
 
 #install nmap
-sudo apt install nmap
+sudo apt install nmap -y
+
+#install tmux
+sudo apt install tmux -y
 
 #install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -42,7 +45,7 @@ cd pwndbg
 sudo apt install checksec
 
 #disable ASLR - this will auto enable after reboot
-echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+#echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 
 #enable ASLR
 #echo 2 | sudo tee /proc/sys/kernel/randomize_va_space
@@ -55,8 +58,8 @@ echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 git config --global http.sslverify "false"
 
 #update CA Cert.
-sudo apt-get install --reinstall ca-certificates
-sudo mkdir /usr/local/share/ca-certificates/cacert.org
-sudo wget -P /usr/local/share/ca-certificates/cacert.org http://www.cacert.org/certs/root.crt http://www.cacert.org/certs/class3.crt
-sudo update-ca-certificates
-git config --global http.sslCAinfo /etc/ssl/certs/ca-certificates.crt
+#sudo apt-get install --reinstall ca-certificates
+#sudo mkdir /usr/local/share/ca-certificates/cacert.org
+#sudo wget -P /usr/local/share/ca-certificates/cacert.org http://www.cacert.org/certs/root.crt http://www.cacert.org/certs/class3.crt
+#sudo update-ca-certificates
+#git config --global http.sslCAinfo /etc/ssl/certs/ca-certificates.crt
